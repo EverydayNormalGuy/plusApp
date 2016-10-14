a508ec293780eeab90ed104f9baf6170006fc328
Everydaynormalguy <metin.g@gmx.net>
2016-10-14 02:31:31 +0200

    - Permissions für die Standortdienste (GPS,WLAN,..) nach Material Design hinzugefügt.

--- app/src/main/java/com/plusapp/pocketbiceps/app/GMapsActivity.java
+++ app/src/main/java/com/plusapp/pocketbiceps/app/GMapsActivity.java
@@ -1,9 +1,20 @@
 package com.plusapp.pocketbiceps.app;
 
+import android.*;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -14,16 +25,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class GMapsActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
 
     private GoogleMap gMap;
+    Context context = this;
+    MarkerDataSource data;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mPlanetTitles;
+    //
+    double latlng;
+    String latlng2;
+    String longitudefordb;
+    String latitudefordb;
 
+    // protected LocationManager locationManager3;
+    LocationListener locationListener;
+
+
+    TextView txtLat;
+    String lat;
+    String provider;
+    protected String latitude3, longitude3;
+    protected boolean gps_enabled, network_enabled;
+
+    double latitude;
+    double longitude;
+    public LocationManager lm;
+
+    private GoogleApiClient googleApiClient;
+    private static final int PERMISSION_ACCESS_COARSE_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +77,15 @@ public class GMapsActivity extends AppCompatActivity
         setContentView(R.layout.activity_gmaps);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        googleApiClient = new GoogleApiClient.Builder(this, this, this).addApi(LocationServices.API).build();
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { android.Manifest.permission.ACCESS_COARSE_LOCATION },
+                    PERMISSION_ACCESS_COARSE_LOCATION);
+        }
+
 
 
 
@@ -55,6 +110,21 @@ public class GMapsActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -64,11 +134,45 @@ public class GMapsActivity extends AppCompatActivity
         }
     }
 
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_ACCESS_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // All good!
+                } else {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.gmaps, menu);
         return true;
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+            double lat = lastLocation.getLatitude(), lon = lastLocation.getLongitude();
+            String units = "imperial";
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 
     @Override
@@ -115,7 +219,48 @@ public class GMapsActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
 
 
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
 
+        String provider = lm.getBestProvider(new Criteria(), true);
+
+
+//
+//        if (ActivityCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            if (context instanceof Activity) {
+//                ActivityCompat.requestPermissions((Activity) context, new String[]{ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+//
+//            }
+//            return;
+//        }
+//
+
+
+
+
+//        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            gMap.setMyLocationEnabled(true);
+//        } else {
+//            // Show rationale and request permission.
+//        }
+
+
+
+//        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 50, (LocationListener) this);
+//
+//
+//
+//        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 50, this);
+
+        data = new MarkerDataSource(context);
+        data.open();
+
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
